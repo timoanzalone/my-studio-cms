eef9b16491324b41ffbe0dce24076d70dadb6f54
timoanzalone <timo.anzalone@gmail.com>
2020-01-22 20:58:34 +0100

    Update blog route?

--- pages/blog/_blog.vue
+++ pages/blog/_blog.vue
@@ -10,7 +10,7 @@ export default {
     if (payload) return { blogPost: payload }
     else
       return {
-        blogPost: await require(`~/assets/content/blog/${params.blog}.json`)
+        blogPost: await require(`~/assets/content/blog/${context.params.blog}.json`)
       }
   }
 }
