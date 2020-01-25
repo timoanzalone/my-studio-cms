9be78658797882385396146da75478ea892f0800
timoanzalone <timo.anzalone@gmail.com>
2020-01-25 16:10:48 +0100

    Update index.vue

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,9 +2,11 @@
   <div class="container mx-auto px-4">
     <div class="flex flex-wrap">
       <div class="px-2" v-for="(blogPost, index) in blogPosts" :key="index">
-        <nuxt-link :to="`/blog/${blogPost.slug}/`">{{blogPost.title}}</nuxt-link>
         <nuxt-link :to="`/blog/${blogPost.slug}/`">
           <img class="w-full object-cover" :src=" blogPost.featured_image " />
+        </nuxt-link>
+        <nuxt-link :to="`/blog/${blogPost.slug}/`">
+          <h2>{{blogPost.title}}</h2>
         </nuxt-link>
         <p>{{blogPost.description}}</p>
       </div>
